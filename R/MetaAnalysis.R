775ec2cb40cd6c608645626acf2bfba435fe6b71
Adam Black <ablack3@users.noreply.github.com>
2020-08-29 17:01:44 -0400

    Fix typo in MetaAnalysis.R described in issue #2

--- R/MetaAnalysis.R
+++ R/MetaAnalysis.R
@@ -216,7 +216,7 @@ sumMinCellCount <- function(counts) {
 }
 
 getAllControls <- function(outputFolder) {
-  pathToCsv <- system.file("settings", "NegativeControls.csv", package = "Covid19EstimationHydroxychloroquine")
+  pathToCsv <- system.file("settings", "NegativeControls.csv", package = "Covid19EstimationHydroxychloroquine2", mustWork = TRUE)
   allControls <- read.csv(pathToCsv)
   allControls$oldOutcomeId <- allControls$outcomeId
   allControls$targetEffectSize <- rep(1, nrow(allControls))
